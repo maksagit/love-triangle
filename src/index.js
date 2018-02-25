7cfdbddb08ab5853318c62a388f0850ba32a785c
maksagit <maksibon@gmail.com>
2018-02-25 16:18:38 +0300

    Decision love triangles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+'use strict';
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  var triangles = 0;
+  var triangleContact= 0;
+  var second, third;
+  //preferences array:
+  preferences.map(function(number, index) {
+    if (number != (index+1)) {
+      second = preferences[number-1];
+      if (preferences[second-1] != second) {
+        third = preferences[second-1];
+        if (preferences[third-1] != third 
+          && (index+1) == third) 
+          triangleContact++;
+        } 
+      }  
+    }
+  );
+  triangles = triangleContact/3;
+  return triangles;
+}
